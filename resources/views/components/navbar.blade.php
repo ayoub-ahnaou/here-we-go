7e0f97282a3dcefacdcda8df4de5168d67af2410
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-02-26 17:13:53 +0000

    Update navbar component to adjust z-index and modify user menu links based on role

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="h-10 w-full bg-gray-100 px-8 text-gray-600 sticky top-0 z-10">
+<nav class="h-10 w-full bg-gray-100 px-8 text-gray-600 sticky top-0 z-50">
     <div class="container mx-auto flex justify-between h-full items-center">
         <a href="{{ route('welcome') }}" class="flex items-center gap-1">
             <img src="{{ URL('assets/icons/logo.svg') }}" class="size-4" alt="" />
@@ -6,7 +6,7 @@
         </a>
 
         <ul class="text-xs flex items-center h-full">
-            @auth
+            {{-- @auth
                 <li class="relative group h-full">
                     <div class="flex items-center gap-1 hover:bg-gray-200 h-full px-2 cursor-pointer">
                         <!-- <img src="./imgs/profile.svg" class="size-5" alt="" /> -->
@@ -20,15 +20,15 @@
                         class="absolute right-0 hidden group-hover:block w-64 bg-white border border-gray-200 rounded-md shadow-lg py-2 mt-1">
                         <!-- Host Section -->
                         <div class="border-b border-gray-100">
-                            <a href="#" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Devenir un
+                            <a href="" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Devenir un
                                 hôte</a>
-                            <a href="#" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes
+                            <a href="" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes
                                 propriétés</a>
-                            <a href="#" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes hôtes</a>
+                            <a href="" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes hôtes</a>
                         </div>
                     </div>
                 </li>
-            @endauth
+            @endauth --}}
 
             <li class="relative group h-full">
                 <div class="flex items-center gap-1 hover:bg-gray-200 h-full px-2 cursor-pointer">
@@ -56,9 +56,17 @@
                         <div class="border-b border-gray-100">
                             <a href="{{ route('profile.edit') }}"
                                 class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mon profil</a>
-                            <a href="#" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes
-                                réservations</a>
-                            <a href="#" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes favoris</a>
+                            @if (Auth::user()->role_id === 2)
+                                <a href="{{ route('annonces.myannonces') }}"
+                                    class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes
+                                    annonces</a>
+                            @elseif (Auth::user()->role_id == 3)
+                                <a href="" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes
+                                    réservations</a>
+                                <a href="{{ route('favoris.index') }}"
+                                    class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Mes
+                                    favoris <span class="text-white bg-red-500 w-4 h-4 rounded-full px-1 text-[10px]"> {{ sizeof(Auth::user()->favoris) ?? 0 }} </span></a>
+                            @endif
                         </div>
                     @endauth
 
@@ -74,7 +82,8 @@
                     <!-- Auth Section -->
                     <div class="">
                         @guest
-                            <a href="{{ route('login') }}" class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Se
+                            <a href="{{ route('login') }}"
+                                class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">Se
                                 connecter</a>
                             <a href="{{ route('register') }}"
                                 class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-50">S'inscrire</a>
@@ -83,7 +92,8 @@
                             <form action="{{ route('logout') }}" method="POST">
                                 @csrf
                                 <input type="submit"
-                                    class="block px-4 w-full cursor-pointer text-left py-2 text-xs text-gray-700 hover:bg-gray-50" value="Déconnexion" />
+                                    class="block px-4 w-full cursor-pointer text-left py-2 text-xs text-gray-700 hover:bg-gray-50"
+                                    value="Déconnexion" />
                             </form>
                         @endauth
                     </div>

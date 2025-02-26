fa056bd08eee03b0ae39f7a2b9ee9ea0ab3a1b40
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-02-26 17:16:12 +0000

    Add annonces relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function annonces()
+    {
+        return $this->hasMany(Annonce::class);
+    }
 }
